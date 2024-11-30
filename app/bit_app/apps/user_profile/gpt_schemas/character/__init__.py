7096802603753a541bd1aae1402ecd5fe45fc427
Bartlomiej <116915393+bl4szk4@users.noreply.github.com>
2024-11-30 21:48:36 +0100

    schemas

---
+++ app/bit_app/apps/user_profile/gpt_schemas/character/__init__.py
@@ -0,0 +1,65 @@
+from bit_app.apps.common.classes import PromptOpenAI
+
+
+SYSTEM_PROMPT = """
+You are a person categorizer. For given person summary create their categorization.
+"""
+
+
+USER_PROMPT = """
+You are given a summary of a person. Decide how each category describes that person in scale 0-5.
+"""
+
+SCHEMA = {
+    "type": "object",
+    "properties": {
+        "traits": {
+            "type": "array",
+            "description": "List of traits with their integer values",
+            "items": {
+                "type": "object",
+                "properties": {
+                    "key": {
+                        "type": "string",
+                        "enum": [
+                            "Adventurousness",
+                            "Conscientiousness",
+                            "Extraversion",
+                            "Neuroticism",
+                            "Openness",
+                            "Perseverance",
+                            "Social Connectedness",
+                            "Curiosity",
+                            "Risk Tolerance",
+                            "Emotional Resonance"
+                        ],
+                        "description": "The name of the trait",
+                    },
+                    "value": {
+                        "type": "integer",
+                        "description": "The value of the trait on a defined scale",
+                    },
+                },
+                "required": ["key", "value"]
+            },
+        },
+    },
+    "required": ["traits"]
+}
+
+
+def result_changing_function(data: dict):
+    result = data.get("traits")
+    data_out = {}
+    for trait in result:
+        data_out[trait["key"]] = trait["value"]
+
+    return data_out
+
+
+HOBBY_SUMMARY = PromptOpenAI(
+    system_prompt=SYSTEM_PROMPT,
+    user_prompt=USER_PROMPT,
+    schema=SCHEMA,
+    prompt_preparing_function=prompt_preparing_function
+)
---
+++ app/bit_app/apps/user_profile/gpt_schemas/summary/__init__.py
@@ -0,0 +1,40 @@
+from bit_app.apps.common.classes import PromptOpenAI
+
+
+SYSTEM_PROMPT = """
+You are a person summarizer. For given list of questions with answers write a summary of that person, including all aspects.
+"""
+
+
+USER_PROMPT = """
+You are given a list of questions that were asked to the user with their answers. Provide me a characteristic of that 
+user. Characteristic should be short and based on those questions with answers.
+"""
+
+SCHEMA = {
+    "type": "object",
+    "properties": {
+        "summary": {
+            "type": "string",
+            "summary": "Summary of the offer",
+        }
+    },
+    "required": ["summary"]
+}
+
+
+def prompt_preparing_function(quiz_data: list[dict]) -> str:
+    data = []
+    for item in quiz_data:
+        key, item = item.items()
+        data.append(f"Question: {key} - answer: {item}")
+
+    return "\n".join(data)
+
+
+USER_SUMMARY = PromptOpenAI(
+    system_prompt=SYSTEM_PROMPT,
+    user_prompt=USER_PROMPT,
+    schema=SCHEMA,
+    prompt_preparing_function=prompt_preparing_function
+)
